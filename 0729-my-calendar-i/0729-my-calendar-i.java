5c9582a0b775a3c1593d7b94430144ec1b5263a1
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2024-09-27 02:19:54 +0530

    Time: 19 ms (81.63%), Space: 45.6 MB (42.53%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,19 +1,26 @@
 class MyCalendar {
-    List<int[]> calendar;
+
+    TreeSet<int[]> st;
+
     public MyCalendar() {
-        calendar = new ArrayList<>();
+        st = new TreeSet<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
     }
-    
+
     public boolean book(int start, int end) {
-        for(int[] val : calendar){
-            if(!(end <= val[0] || start >= val[1])){
-                return false;
-            }
+        int[] event = new int[]{start, end};
+        int[] next = st.ceiling(event); 
+        if (next != null && next[0] < end) {
+            return false;
         }
-        calendar.add(new int[]{start, end});
+        int[] prev = st.floor(event);
+        if (prev != null && start < prev[1]) {
+            return false;
+        }
+        st.add(new int[]{start, end});
         return true;
     }
 }
+
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
