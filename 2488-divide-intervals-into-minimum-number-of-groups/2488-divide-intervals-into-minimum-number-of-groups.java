7b8bb9187527d8713d9115bedda5b38ff6c247b7
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2024-10-13 03:04:20 +0530

    Time: 27 ms (94.56%), Space: 94.5 MB (19.43%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
@@ -1,15 +1,24 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int[] interval : intervals) {
-            int start = interval[0];
-            int end = interval[1];
-            if (!pq.isEmpty() && pq.peek() < start) {
-                pq.poll();
-            }
-            pq.offer(end);
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int n = intervals.length;
+        for(int i=0;i<n;i++){
+            min = Math.min(min, intervals[i][0]);
+            max = Math.max(max, intervals[i][1]);
+        }  
+        int eventCount[] = new int[max+2];
+        // find the count
+        for(int i=0;i<n;i++){
+            eventCount[intervals[i][0]]++;
+            eventCount[intervals[i][1]+1]--;
+        } 
+        int maxOverlaps = 0;
+        int sum=0;
+        for(int i=min;i<max+2;i++){
+            sum+=eventCount[i];
+            maxOverlaps = Math.max(maxOverlaps, sum);
         }
-        return pq.size();
+        return maxOverlaps;
     }
 }
