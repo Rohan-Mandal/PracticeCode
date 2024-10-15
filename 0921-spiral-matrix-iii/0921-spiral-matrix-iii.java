afa71a79a5e6ecbfb737a9f8f4035085f9883077
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2024-10-16 02:46:35 +0530

    Time: 3 ms (96.75%), Space: 45.2 MB (62.27%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.java
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.java
@@ -1,31 +1,30 @@
 class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int[][] directions = {
-            {0, 1}, // --> EAST
-            {1, 0}, // --> SOUTH
-            {0, -1},  // --> EAST
-            {-1, 0}  // --> NORTH
-        };
-
-        int[][] result = new int[rows * cols][2];
-        int steps = 0;
-        int dir = 0;
-        result[0] = new int[]{rStart, cStart};
-        int count = 1;
-        while(count < rows * cols){
-            if(dir == 0 || dir == 2){
-                steps++;
-            }
-            for(int i = 0; i < steps; i++){
-                rStart += directions[dir][0];
-                cStart += directions[dir][1];
-
-                if(rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols){
-                    result[count++] = new int[]{rStart, cStart};
-                }
-            }
-            dir = (dir + 1) % 4;
-        }
-        return result;
+        int directions[][] = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+		int n = rows * cols;
+		int res[][] = new int[n][2];
+		res[0][0] = rStart;
+		res[0][1] = cStart;
+		int count = 1;
+		int steps = 1;
+		int indx = 0;
+		while (count < n) {
+			for (int times = 0; times < 2; times++) {
+				int dr = directions[indx % 4][0];
+				int dc = directions[indx % 4][1];
+				for (int i = 0; i < steps; i++) {
+					rStart += dr;
+					cStart += dc;
+					if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols) {
+						res[count][0] = rStart;
+						res[count][1] = cStart;
+						count++;
+					}
+				}
+				indx++;
+			}
+			steps++;
+		}
+		return res;
     }
 }
