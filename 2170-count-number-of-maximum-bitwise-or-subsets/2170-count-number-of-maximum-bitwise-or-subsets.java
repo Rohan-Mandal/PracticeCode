b27204a8f04e3c47ac92c1b60ef5582c179a1dab
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2024-10-19 02:26:33 +0530

    Time: 8 ms (86.08%), Space: 40.8 MB (86.08%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -2,30 +2,26 @@ class Solution {
 
     public int countMaxOrSubsets(int[] nums) {
         int maxOrValue = 0;
-        int maxOrCount = 0;
-
-        // There are 2^n subsets for an array of length n
-        int totalSubsets = 1 << nums.length;
-
-        // Iterate through all possible subsets using bit manipulation
-        for (int i = 1; i < totalSubsets; i++) {
-            int currentOrValue = 0;
-            for (int j = 0; j < nums.length; j++) {
-                // Check if the j-th bit in i is set; if so, include nums[j] in the subset
-                if ((i & (1 << j)) != 0) {
-                    currentOrValue |= nums[j];
-                }
-            }
-
-            // Update maximum OR value and count occurrences of this maximum value
-            if (currentOrValue > maxOrValue) {
-                maxOrValue = currentOrValue;
-                maxOrCount = 1; // Reset the count since we found a new maximum
-            } else if (currentOrValue == maxOrValue) {
-                maxOrCount++; // Increment the count if current OR equals the max OR value
-            }
+        for(int num : nums){
+            maxOrValue |= num;
         }
-        return maxOrCount;
+        int currOrValue = 0;
+        return countSubsets(0, currOrValue, maxOrValue, nums);
+    }
 
+    private int countSubsets(int index, int currOrValue, int maxOrValue, int[] nums)
+    {
+        if(index == nums.length){
+            if(currOrValue == maxOrValue)
+                return 1;
+            return 0;
+        }
+        // take nums[index]
+        int takeCount = countSubsets(index + 1, currOrValue | nums[index], maxOrValue, nums);
+
+        //not taken nums[index]
+        int notTakenCount = countSubsets(index + 1, currOrValue, maxOrValue, nums);
+
+        return takeCount + notTakenCount;
     }
 }
