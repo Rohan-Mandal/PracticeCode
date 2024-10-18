481033750dfab04e6b4b113fd95d6b0c7d49d689
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2024-10-19 05:07:31 +0530

    Time: 1 ms (99.93%), Space: 43.4 MB (49.37%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,41 +1,21 @@
-import java.util.*;
 class Solution {
-
-    List<List<Integer>> result = new ArrayList<>();
-
-
-    private void solve(int[] nums, int index, List<Integer> temp){
-
-        if(index >= nums.length){
-            result.add(new ArrayList<>(temp));
-            return;
-        }
-
-        // with element
-        temp.add(nums[index]);
-        solve(nums, index + 1, temp);
-        //without element
-        temp.remove(temp.size() - 1);
-        while(index + 1 < nums.length && nums[index] == nums[index + 1]){
-            index++;
-        }
-        solve(nums, index + 1, temp);
-    }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        List<Integer> temp = new ArrayList<>();
-        solve(nums, 0, temp);
-        Comparator<List<Integer>> listComparator = (list1, list2) -> {
-            for (int i = 0; i < list1.size(); i++) {
-                if (list1.get(i) != list2.get(i)) {
-                    return list1.get(i) - list2.get(i);
-                }
-            }
-            return 0; // If all elements are equal, return 0
-        };
-
-        Collections.sort(result, listComparator);
+        solve(0, nums, new ArrayList<>(), result);
         return result;
     }
 
+    private void solve(int index, int[] nums, List<Integer> temp,List<List<Integer>> result){
+        result.add(new ArrayList<>(temp));
+        for(int i = index; i < nums.length; i++){
+            if(i > index && nums[i] == nums[i - 1]){
+                continue;
+            }
+            temp.add(nums[i]);
+            solve(i + 1, nums, temp, result);
+            temp.remove(temp.size() - 1);
+        }
+    }
 }
+
