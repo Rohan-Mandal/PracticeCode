b0c856eed04b5f669401d48ba6c2c765caa7a066
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2024-10-20 04:33:19 +0530

    Time: 0 ms (100%), Space: 40.5 MB (21.61%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.java
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.java
@@ -1,15 +1,19 @@
 class Solution {
     public int kthGrammar(int n, int k) {
+
+        // if n & k == 1 return
         if(n == 1 && k == 1){
             return 0;
         }
+
+        // for n = 2^n
         int len = (int)(Math.pow(2, n - 1) / 2);
 
         if(k <= len){
-            return kthGrammar(n - 1, k);
+            return kthGrammar(n - 1, k); // if k <= len, anser lies in the previous answer
         }else{
-            int val = kthGrammar(n - 1, k - len);
-            return (val == 0) ? 1 : 0;
+            int val = kthGrammar(n - 1, k - len); // if k > len, then (mid + 1)th element is reverse of 1st element, 
+            return (val == 0) ? 1 : 0;           //(mid + 2)th element is reverse of 2nd element and so on.
         }
     }
 }
