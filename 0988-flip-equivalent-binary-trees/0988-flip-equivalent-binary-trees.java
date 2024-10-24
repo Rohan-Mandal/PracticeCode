44c9e65a52a144a5580ccc43115adc56c7414f3c
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2024-10-25 01:43:54 +0530

    Time: 0 ms (100%), Space: 41.1 MB (71.43%) - LeetHub

--- 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.java
+++ 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.java
@@ -22,9 +22,7 @@ class Solution {
             return false;
         }
         if(root1.val == root2.val){
-            boolean checkWithoutFlip = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
-            boolean checkWithFlip = flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
-            return  checkWithoutFlip || checkWithFlip;
+            return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
         }
         return false;
     }
