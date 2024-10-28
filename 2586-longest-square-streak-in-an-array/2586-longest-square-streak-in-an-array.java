3b51d2681267f64a4b437b1e504b1d29d1361927
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2024-10-28 23:38:50 +0530

    Time: 48 ms (56.25%), Space: 61 MB (44.44%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.java
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.java
@@ -1,29 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int longestSquareStreak(int[] nums) {
-         Set<Integer> st = new HashSet<>();
+        Set<Long> set = new HashSet<>();
         for (int num : nums) {
-            st.add(num);
+            set.add((long) num);
         }
 
         int maxStreak = 0;
-
         for (int num : nums) {
-            int streak = 0;
-            long curr = num;
-
-            while (st.contains((int) curr)) {
-                streak++;
-
-                if (curr * curr > 1e5) {
-                    break;
-                }
-
-                curr = curr * curr; // square
+            int count = 0;
+            long current = num;
+            while (set.contains(current)) {
+                count++;
+                current = current * current;
+                if (current > Integer.MAX_VALUE) break; // Avoid overflow
             }
-
-            maxStreak = Math.max(maxStreak, streak);
+            maxStreak = Math.max(maxStreak, count);
         }
 
-        return maxStreak < 2 ? -1 : maxStreak;
+        return maxStreak >= 2 ? maxStreak : -1;
     }
 }
