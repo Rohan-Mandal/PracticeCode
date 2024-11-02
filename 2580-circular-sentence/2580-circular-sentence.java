bfa5791e853baa76b8338791ddf5c8cf3e70f3c4
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2024-11-02 13:08:22 +0530

    Time: 1 ms (96.81%), Space: 42.1 MB (37.29%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.java
+++ 2580-circular-sentence/2580-circular-sentence.java
@@ -1,11 +1,8 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        // First, check if the sentence itself is circular by comparing the first and last characters
         if (sentence.charAt(0) != sentence.charAt(sentence.length() - 1)) {
             return false;
         }
-
-        // Split the sentence by spaces to check each word connection
         String[] words = sentence.split(" ");
         
         // Check if the last character of each word matches the first character of the next word
@@ -16,7 +13,6 @@ class Solution {
                 return false;
             }
         }
-
         // If all conditions are met, the sentence is circular
         return true;
     }
