1a05f1d09eb82f5a5a5ed920ef1c0fade44f60cd
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2024-12-04 21:10:50 +0530

    Time: 0 ms (100%), Space: 40.1 MB (74.07%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,11 +1,5 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
-        int result = ((a | b) ^ c); // flips
-        
-        // if both bit is 1 in a & b
-        int temp = (a & b);
-        int result2 = (result & temp);
-
-        return Integer.bitCount(result) + Integer.bitCount(result2);
+        return Integer.bitCount((a | b) ^ c) + Integer.bitCount((a & b) & ((a | b) ^ c));
     }
 }
