cc3e1af8350387545dc5fdda14fa9915231852cd
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-02-09 00:01:36 +0530

    Time: 56 ms (96.58%), Space: 100.2 MB (32.88%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.java
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.java
@@ -1,7 +1,7 @@
 class NumberContainers {
-    
+    // Space : O(n)
     private Map<Integer, Integer> idxToNum;
-    private Map<Integer, TreeSet<Integer>> numToIdx;
+    private Map<Integer, PriorityQueue<Integer>> numToIdx;
 
     public NumberContainers() {
         idxToNum = new HashMap<>();
@@ -9,22 +9,24 @@ class NumberContainers {
     }
     
     public void change(int index, int number) {
-        if(idxToNum.containsKey(index)) { // O(1)
-            int prevNum = idxToNum.get(index);
-            numToIdx.get(prevNum).remove(index); // O(log(n))
-            if(numToIdx.get(prevNum).isEmpty()) {
-                numToIdx.remove(prevNum); // O(1)
-            }
-        }
-
-        idxToNum.put(index, number);
-        numToIdx.putIfAbsent(number, new TreeSet<>());
-        numToIdx.get(number).add(index); // O(log(n))
+        idxToNum.put(index, number); // O(1)
+        numToIdx.putIfAbsent(number, new PriorityQueue<>());
+        numToIdx.get(number).offer(index); // O(log(n))
     }
     
     public int find(int number) {
-        if(numToIdx.containsKey(number)) { // O(1)
-            return numToIdx.get(number).first(); // O(1)
+        if (!numToIdx.containsKey(number)) {
+            return -1;
+        }
+
+        PriorityQueue<Integer> pq = numToIdx.get(number);
+
+        while (!pq.isEmpty()) { // O(k * logn)
+            int idx = pq.peek(); // O(1)
+            if (idxToNum.get(idx) == number) {
+                return idx;
+            }
+            pq.poll(); // O(log(n))
         }
         return -1;
     }
