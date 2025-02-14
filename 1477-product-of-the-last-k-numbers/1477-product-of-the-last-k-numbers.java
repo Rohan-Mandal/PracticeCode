5d68a31cc901d6bc57d76d0245b1bbeeee857508
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-02-15 01:08:42 +0530

    Time: 17 ms (33.97%), Space: 69 MB (53.2%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
@@ -1,25 +1,29 @@
 class ProductOfNumbers {
-
-    private List<Integer> numbers;
-
+    private List<Integer> prefixProduct;
+    
     public ProductOfNumbers() {
-        numbers = new ArrayList<>();
+        prefixProduct = new ArrayList<>();
+        prefixProduct.add(1); // Initializing with 1 for easier division calculations
     }
     
     public void add(int num) {
-        numbers.add(num);
+        if (num == 0) {
+            // Reset on encountering 0, as it invalidates previous products
+            prefixProduct.clear();
+            prefixProduct.add(1);
+        } else {
+            int lastProduct = prefixProduct.get(prefixProduct.size() - 1);
+            prefixProduct.add(lastProduct * num);
+        }
     }
     
     public int getProduct(int k) {
-        int size = numbers.size();
-        int product = 1;
-
-        for(int i = size - k; i < size; i++){
-            product *= numbers.get(i);
-        }
-        return product;
+        int size = prefixProduct.size();
+        if (k >= size) return 0; // If k is larger than the available numbers (due to reset), return 0
+        return prefixProduct.get(size - 1) / prefixProduct.get(size - 1 - k);
     }
 }
+
 
 /**
  * Your ProductOfNumbers object will be instantiated and called as such:
