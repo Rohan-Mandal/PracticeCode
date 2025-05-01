35c43aef18dec37274ab1da6386f9074f5b1fb1c
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-05-01 22:22:00 +0530

    Time: 64 ms (76.33%), Space: 55.8 MB (53.88%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.java
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.java
@@ -1,51 +1,56 @@
 class Solution {
-    public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
+
+    public int maxTaskAssign(
+        int[] tasks,
+        int[] workers,
+        int pills,
+        int strength
+    ) {
+        int n = tasks.length, m = workers.length;
         Arrays.sort(tasks);
         Arrays.sort(workers);
-        int n = tasks.length;
-        int m = workers.length;
         int left = 1, right = Math.min(m, n), ans = 0;
-
-        while(left <= right){
+        while (left <= right) {
             int mid = (left + right) / 2;
-            if(check(tasks, workers, pills, strength, mid)){
+            if (check(tasks, workers, pills, strength, mid)) {
                 ans = mid;
                 left = mid + 1;
-            } else{
+            } else {
                 right = mid - 1;
             }
         }
         return ans;
     }
 
-    private boolean check(int[] tasks, int[] workers, int pills, int strength, int mid){
+    // Check if pills and strength can be used in mid tasks
+    private boolean check(
+        int[] tasks,
+        int[] workers,
+        int pills,
+        int strength,
+        int mid
+    ) {
         int p = pills;
-        int n = workers.length;
-        TreeMap<Integer, Integer> workersMap = new TreeMap<>();
-        for(int i = n - mid; i < n; ++i){
-            workersMap.put(workers[i], workersMap.getOrDefault(workers[i], 0) + 1);
-        }
-
-        for(int i = mid - 1; i >= 0; --i){
-            Integer key = workersMap.lastKey();
-            if(key >= tasks[i]){
-                workersMap.put(key, workersMap.get(key) - 1);
-                if(workersMap.get(key) == 0){
-                    workersMap.remove(key);
-                }
-            } else{
-                if(p == 0){
+        int m = workers.length;
+        Deque<Integer> ws = new ArrayDeque<>();
+        int ptr = m - 1;
+        // Enumerate each task from largest to smallest
+        for (int i = mid - 1; i >= 0; --i) {
+            while (ptr >= m - mid && workers[ptr] + strength >= tasks[i]) {
+                ws.addFirst(workers[ptr]);
+                --ptr;
+            }
+            if (ws.isEmpty()) {
+                return false;
+            } else if (ws.getLast() >= tasks[i]) {
+                // If the largest element in the deque is greater than or equal to tasks[i]
+                ws.pollLast();
+            } else {
+                if (p == 0) {
                     return false;
-                }
-                key = workersMap.ceilingKey(tasks[i] - strength);
-                if(key == null){
-                    return false;
-                }
-                workersMap.put(key, workersMap.get(key) - 1);
-                if(workersMap.get(key) == 0){
-                    workersMap.remove(key);
                 }
                 --p;
+                ws.pollFirst();
             }
         }
         return true;
