25493566814a4ed11060114fa49bfce60ce14556
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-05-25 22:06:55 +0530

    Time: 116 ms (39.02%), Space: 57.1 MB (83.94%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
@@ -2,27 +2,30 @@ class Solution {
     public int longestPalindrome(String[] words) {
         Map<String, Integer> map = new HashMap<>();
         int result = 0;
+        boolean hasMiddle = false;
 
-        for(String word : words){
+        for (String word : words) {
             String reversed = new StringBuilder(word).reverse().toString();
-            if(map.getOrDefault(reversed, 0) > 0){
-                result += 4;
+            if (map.getOrDefault(reversed, 0) > 0) {
+                result += 4;  // each pair adds 4 characters
                 map.put(reversed, map.get(reversed) - 1);
-            } else{
+            } else {
                 map.put(word, map.getOrDefault(word, 0) + 1);
             }
         }
-        
-        // check for a same character word to put in middle
-        for(Map.Entry<String, Integer> entry : map.entrySet()){
+
+        // check if there is a word like "gg" that can be used in the middle
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
             String word = entry.getKey();
             int count = entry.getValue();
-
-            if(word.charAt(0) == word.charAt(1) && count > 0){
-                result += 2;
+            if (word.charAt(0) == word.charAt(1) && count > 0) {
+                hasMiddle = true;
                 break;
             }
         }
+
+        if (hasMiddle) result += 2; // add 1 self-palindrome word in center
+
         return result;
     }
 }
