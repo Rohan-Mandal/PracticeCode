831a6dd273cec359ca4bb758fbffc1b0e6afc5ef
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-07-03 01:34:36 +0530

    Time: 86 ms (97.3%), Space: 55.8 MB (43.24%) - LeetHub

--- 3618-find-the-original-typed-string-ii/3618-find-the-original-typed-string-ii.java
+++ 3618-find-the-original-typed-string-ii/3618-find-the-original-typed-string-ii.java
@@ -1,48 +1,36 @@
 class Solution {
+    private static final long MOD = (long)1e9 + 7;
     public int possibleStringCount(String word, int k) {
-        final int MODULO = (int) (1e9 + 7);
-        int wordLength = word.length();
-        List<Integer> characterGroups = new ArrayList<>();
-        
-        for (int index = 0; index < wordLength;) {
-            int characterCount = 1;
-            char currentCharacter = word.charAt(index++);
-            while (index < wordLength && word.charAt(index) == currentCharacter) {
-                characterCount++;
-                index++;
+        List<Integer> list = new ArrayList<>();
+        int n = word.length();
+        int i = 0;
+        while (i < n) {
+            int j = i+1;
+            while (j < n && word.charAt(j) == word.charAt(j-1)) j++;
+            list.add(j - i);
+            i = j;
+        }
+        int m = list.size();
+        long[] power = new long[m];
+        power[m-1] = list.get(m-1);
+        for (i = m-2; i >= 0; i--) {
+            power[i] = (power[i+1] * list.get(i)) % MOD;
+        }
+        if (m >= k) return (int)power[0];
+
+        long[][] dp = new long[m][k-m+1];
+        for (i = 0; i < k-m+1; i++) {
+            if (list.get(m-1) + i + m > k) dp[m-1][i] = list.get(m-1) - (k-m-i);
+        }
+        for (i = m-2; i >= 0; i--) {
+            long sum = (dp[i+1][k-m] * list.get(i)) % MOD;
+            for (int j = k-m; j >= 0; j--) {
+                sum += dp[i+1][j];
+                if (j + list.get(i) > k-m) sum = (sum - dp[i+1][k-m] + MOD) % MOD;
+                else sum = (sum - dp[i+1][j+list.get(i)] + MOD) % MOD;
+                dp[i][j] = sum;
             }
-            characterGroups.add(characterCount);
         }
-
-        long totalCombinations = 1;
-        for (int groupCount : characterGroups) {
-            totalCombinations = (totalCombinations * groupCount) % MODULO;
-        }
-
-        if (k <= characterGroups.size()) {
-            return (int) totalCombinations;
-        }
-
-        int maxSize = k - 1;
-        int[] dp = new int[maxSize + 1];
-        dp[0] = 1;
-
-        for (int count : characterGroups) {
-            int[] newDP = new int[maxSize + 1];
-            long cumulativeSum = 0;
-            for (int s = 0; s <= maxSize; s++) {
-                if (s - 1 >= 0) cumulativeSum = (cumulativeSum + dp[s - 1]) % MODULO;
-                if (s - count - 1 >= 0) cumulativeSum = (cumulativeSum - dp[s - count - 1] + MODULO) % MODULO;
-                newDP[s] = (int) cumulativeSum;
-            }
-            dp = newDP;
-        }
-
-        long totalLessThanK = 0;
-        for (int s = characterGroups.size(); s <= maxSize; s++) {
-            totalLessThanK = (totalLessThanK + dp[s]) % MODULO;
-        }
-
-        return (int) ((totalCombinations - totalLessThanK + MODULO) % MODULO);
+        return (int)dp[0][0];
     }
 }
