c7d7e317074cb8b87b51c66b1cbf6bae3902eeaa
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-07-14 01:52:13 +0530

    Time: 26 ms (66.05%), Space: 57.6 MB (79.31%) - LeetHub

---
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int matchCount = 0;
+        
+        int i = 0, j = 0;
+        while(i < players.length && j < trainers.length){
+            if(players[i] <= trainers[j]){ // player mathces or can match the trainers capacity
+                matchCount++;
+                i++;
+                j++;
+            } else{
+                j++; // trainer is weak
+            }
+        }
+        return matchCount;
+    }
+}
