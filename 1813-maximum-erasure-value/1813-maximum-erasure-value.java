3c3823dadfd807b7a9f0578cee5179898c71fb64
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-07-23 00:29:47 +0530

    Time: 6 ms (97.88%), Space: 60.3 MB (52.07%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.java
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.java
@@ -1,23 +1,24 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
+        int ans = 0;
+
+        int[] set = new int[10001]; // since nums[i] <= 10^4
+
+        int sum = 0;
         int n = nums.length;
-        int i = 0, j = 0;
-        int sum = 0, maxSum = 0;
+        int left = 0;
 
-        Set<Integer> set = new HashSet<>();
-
-        while(j < n){
-            if(!set.contains(nums[j])){
-                sum += nums[j];
-                set.add(nums[j]);
-                maxSum = Math.max(maxSum, sum);
-                j++;
-            } else{
-                set.remove(nums[i]);
-                sum -= nums[i];
-                i++;
+        for (int right = 0; right < n; right++) {
+            int cur = nums[right];
+            sum += cur;
+            set[cur]++;
+            while (set[cur] > 1) { // if the frequency of the element is > 1,
+                sum -= nums[left]; // we subtract it from sum
+                set[nums[left]]--; // decrease the frequency
+                left++;           // move the pointer to right
             }
+            ans = Math.max(ans, sum);
         }
-        return maxSum;
+        return ans;
     }
 }
