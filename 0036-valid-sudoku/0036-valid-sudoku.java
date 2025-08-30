a9d7eead87a9eb67c277370ac6780340f0b9c621
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-08-31 02:30:23 +0530

    Time: 15 ms (8.44%), Space: 45 MB (23.46%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,62 +1,24 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // Validate rows
-        for(int row = 0; row < 9; row ++){
-            Set<Character> set = new HashSet<>();
-            for(int col = 0; col < 9; col++){
-                char ch = board[row][col];
-                if(ch == '.'){
-                    continue;
-                }
-                if(set.contains(ch)){
+        Set<String> seen = new HashSet<>();
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                
+                String row = board[i][j] + "_row_" + i;
+                String col = board[i][j] + "_col_" + j;
+                String box = board[i][j] + "_box_" + (i / 3) + "_" + (j / 3);
+                
+                if (seen.contains(row) || seen.contains(col) || seen.contains(box)) {
                     return false;
                 }
-                set.add(ch);
+                seen.add(row);
+                seen.add(col);
+                seen.add(box);
             }
         }
-
-        // Validate col
-        for(int col = 0; col < 9; col++){
-            Set<Character> set = new HashSet<>();
-            for(int row = 0; row < 9; row++){
-                char ch = board[row][col];
-                if(ch == '.'){
-                    continue;
-                }
-                if(set.contains(ch)){
-                    return false;
-                }
-                set.add(ch);
-            }
-        }
-
-        // Validate 3*3 boxes
-        for(int sr = 0; sr < 9; sr += 3){
-            int er = sr + 2;
-            for(int sc = 0; sc < 9; sc += 3){
-                int ec = sc + 2;
-                if(!validBox(board, sr, er, sc, ec)){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private boolean validBox(char[][] board, int sr, int er, int sc, int ec){
-        Set<Character> set = new HashSet<>();
-        for(int row = sr; row <= er; row++){
-            for(int col = sc; col <= ec; col++){
-                char ch = board[row][col];
-                if(ch == '.'){
-                    continue;
-                }
-                if(set.contains(ch)){
-                    return false;
-                }
-                set.add(ch);
-            }
-        }
+        
         return true;
     }
 }
