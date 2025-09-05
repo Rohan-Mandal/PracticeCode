c325b7ac97d27b10a47751f43416aa6dcc6f2e63
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-09-06 02:18:31 +0530

    Time: 2 ms (97.82%), Space: 43.6 MB (61.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,39 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
+
+        // Approach 1 :- Sort and Compare
+        // Time : O(n log n) (where n = s.length).
+        // Space: O(1)
+
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // char [] sChar = s.toCharArray();
+        // char[] tChar = t.toCharArray();
+        // Arrays.sort(sChar);
+        // Arrays.sort(tChar);
+
+        // return Arrays.equals(sChar, tChar);
+
+        if (s.length() != t.length()) {
             return false;
         }
-        char [] sChar = s.toCharArray();
-        char[] tChar = t.toCharArray();
-        Arrays.sort(sChar);
-        Arrays.sort(tChar);
 
-        return Arrays.equals(sChar, tChar);
+        int[] freq = new int[26];
+
+        for(char c : s.toCharArray()){
+            freq[c - 'a']++;
+        }
+
+        for(char c : t.toCharArray()){
+            freq[c - 'a']--;
+        }
+
+        for(int count : freq){
+            if(count != 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
