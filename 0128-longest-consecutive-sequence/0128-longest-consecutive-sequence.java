c4fa35976963e2ff09c549b3c2a178177e823a23
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-09-08 01:45:57 +0530

    Time: 33 ms (30.84%), Space: 67.9 MB (22.11%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -3,23 +3,50 @@ class Solution {
 
         //Approach 1 : Sorting + two pointers
         //Time :- n(log n)
-        int n = nums.length;
-        if(n == 0) return 0;
+        // int n = nums.length;
+        // if(n == 0) return 0;
 
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+
+        // int maxLen = 1;
+        // int currLen = 1;
+
+        // for(int i = 1; i < n; i++){
+        //     if(nums[i] == nums[i - 1]){
+        //         continue;
+        //     } else if(nums[i] == nums[i - 1] + 1){
+        //         currLen++;
+        //     } else{
+        //         currLen = 1;
+        //     }
+        //     maxLen = Math.max(maxLen, currLen);
+        // }
+        // return maxLen;
+
+        // Approach 2: using HashSet
+        // Time :O(n)
+
+        if (nums.length == 0)
+            return 0;
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
 
         int maxLen = 1;
-        int currLen = 1;
 
-        for(int i = 1; i < n; i++){
-            if(nums[i] == nums[i - 1]){
-                continue;
-            } else if(nums[i] == nums[i - 1] + 1){
-                currLen++;
-            } else{
-                currLen = 1;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currNum = num;
+                int streak = 1;
+
+                while (set.contains(currNum + 1)) {
+                    currNum++;
+                    streak++;
+                }
+                maxLen = Math.max(maxLen, streak);
             }
-            maxLen = Math.max(maxLen, currLen);
         }
         return maxLen;
     }
