8ad498b06a37eb510a1d767d47d5780f74157e5f
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-09-16 02:48:15 +0530

    Time: 2 ms (91.31%), Space: 42.7 MB (39.44%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -1,6 +1,25 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
 
+        // Appraoch 1
+        // String[] words = text.split(" ");
+        // int count = 0;
+        // for(String word : words){
+        //     boolean canType = true;
+
+        //     for(char ch : brokenLetters.toCharArray()){
+        //         if(word.indexOf(ch) != -1){
+        //             canType = false;
+        //             break;
+        //         }
+        //     }
+        //     if(canType){
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+        // Approach 2
         String[] words = text.split(" ");
         Set<Character> brokenSet = new HashSet<>();
 
