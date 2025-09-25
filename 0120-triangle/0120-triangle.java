7e79999f815b9536b556369057df778f97192e06
Rohan-Mandal <81823834+Rohan-Mandal@users.noreply.github.com>
2025-09-26 02:46:37 +0530

    Time: 9 ms (11.05%), Space: 45.7 MB (20.68%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,26 +1,42 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
+
+        // Appraoch 1: Recursion & memoization
+        //     int n = triangle.size();
+        //     Integer[][] dp = new Integer[n][n];
+        //     return dfs(triangle, 0, 0, dp);
+        // }
+
+        // private int dfs(List<List<Integer>> triangle, int row, int col, Integer[][] dp){
+        //     // base case: last row
+        //     if(row == triangle.size() - 1){
+        //         return triangle.get(row).get(col);
+        //     }
+
+        //     // If already computed, return cached value
+        //     if(dp[row][col] != null){
+        //         return dp[row][col];
+        //     }
+
+        //     // recursive calls
+        //     int down = dfs(triangle, row + 1, col, dp);
+        //     int downAdjc = dfs(triangle, row + 1, col + 1, dp);
+        //     dp[row][col] = triangle.get(row).get(col) + Math.min(down, downAdjc);
+
+        //     return dp[row][col];
+
+        // Approach 2:- Bottom-up (In-place)
         int n = triangle.size();
-        Integer[][] dp = new Integer[n][n];
-        return dfs(triangle, 0, 0, dp);
-    }
 
-    private int dfs(List<List<Integer>> triangle, int row, int col, Integer[][] dp){
-        // base case: last row
-        if(row == triangle.size() - 1){
-            return triangle.get(row).get(col);
+        // Start from second last row, move upwards
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col < triangle.get(row).size(); col++) {
+                int up = triangle.get(row + 1).get(col);
+                int upDiag = triangle.get(row + 1).get(col + 1);
+                int minVal = triangle.get(row).get(col) + Math.min(up, upDiag);
+                triangle.get(row).set(col, minVal);
+            }
         }
-
-        // If already computed, return cached value
-        if(dp[row][col] != null){
-            return dp[row][col];
-        }
-
-        // recursive calls
-        int down = dfs(triangle, row + 1, col, dp);
-        int downAdjc = dfs(triangle, row + 1, col + 1, dp);
-        dp[row][col] = triangle.get(row).get(col) + Math.min(down, downAdjc);
-
-        return dp[row][col];
+        return triangle.get(0).get(0);
     }
 }
